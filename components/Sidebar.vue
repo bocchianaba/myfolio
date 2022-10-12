3b987e43812f30fbe8e22268a894ca5d1aa1be81
bocchianaba <45669998+bocchianaba@users.noreply.github.com>
2022-10-12 06:25:36 +0100

    initialisation of the portfolio project

---
+++ components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>This will be the sidebar component</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Sidebar',
+})
+</script>

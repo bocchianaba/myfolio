427bd7e7dccc5312fd8af99b577738308a038fb4
bocchianaba <45669998+bocchianaba@users.noreply.github.com>
2022-10-12 06:26:07 +0100

    initialisation

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  <Sidebar/>
+  <div> Hi I"m Bocchi Anaba</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
